027bdd2d14a859be413298bf203a519db97b49a2
“catrifoot” <catrifoot@gmail.com>
2020-12-28 10:07:46 +0100

    git commit

---
+++ 06-js-fundamentals/hello-world.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* 
+This is a hello world application that shows alerts.
+*/
+
+ /* this shows a popup with the number 6 */
+ // alert (3 + 1 + 2);// the sum will be performed before the alert
+
+ // alert ('Hello world');
+
+/* declaring a variable */
+ let message;
+ message = 'Goodbye world'
+
+ /*assign a value to the variable */
+ message = 'Hello world!';
+
+ /* referencing the variable */
+ alert(message);
+ alert(message);
